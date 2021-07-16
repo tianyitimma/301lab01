2f5f5b9ea63e1a18f6dad74bfa94a5bfbf525879
mtygit <tim.ma0118@gmail.com>
2021-07-15 17:38:19 -0700

    finally get the features for modal with the help of demo code

--- src/main.js
+++ src/main.js
@@ -3,26 +3,20 @@ import HornedBeast from './HornedBeast';
 
 
 class Main extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
 
-  }
 
   render () {
     return (
       <div className="main">
-        {this.props.beast.map(item =>
+        {this.props.allBeast.map((item, idx) => (
           <HornedBeast
-            id={item.index}
+            key={idx}
             title={item.title}
             image_url={item.image_url}
             description={item.description}
-
+            turnOnModal={this.props.turnOnModal}
           />
-        )}
+        ))}
       </div>
     );
   }
